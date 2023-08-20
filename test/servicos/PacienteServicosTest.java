c97465564720c74e41a881b36fa804037ca34a94
RafaelLuz <rafael.luz2005@gmail.com>
2023-08-20 16:29:55 -0300

    Realizando testes com jUnit

---
+++ test/servicos/PacienteServicosTest.java
@@ -0,0 +1,67 @@
+package servicos;
+
+import dao.PacienteDAO;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import modelo.Paciente;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PacienteServicosTest {
+    
+    private PacienteDAO pacienteDao;
+    
+    public PacienteServicosTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testarBuscaNomePaciente() throws SQLException  {
+        Paciente pacienteEncontrado = pacienteDao.consultar(1);
+        assertEquals("Maria da Silva", pacienteEncontrado.getNome());
+    }
+    
+    @Test
+    public void testarBuscaCpfPaciente() throws SQLException {
+        Paciente pacienteEncontrado = pacienteDao.consultar(1);
+        assertEquals("123.456.789-00", pacienteEncontrado.getCpf());
+    }
+    
+    @Test
+    public void testarBuscaEnderecoPaciente() throws SQLException {
+        Paciente pacienteEncontrado = pacienteDao.consultar(1);
+        assertEquals("Rua A, 123", pacienteEncontrado.getEndereco());
+    }
+    
+    @Test
+    public void testarBuscaTelefonePaciente() throws SQLException {
+        Paciente pacienteEncontrado = pacienteDao.consultar(1);
+        assertEquals("(11) 1234-5678", pacienteEncontrado.getTelefone());
+    }
+    
+    @Test
+    public void testarBuscaDataNascPaciente() throws SQLException {
+        Paciente pacienteEncontrado = pacienteDao.consultar(1);
+        assertEquals("1990-01-01", pacienteEncontrado.getDataNascimento());
+    }
+    
+}
